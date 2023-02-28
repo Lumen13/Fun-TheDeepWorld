e104853276b1772112312e3b125a75f56f7877c7
Lumen13 <ntap13@gmail.com>
2023-02-28 18:45:16 +0300

    Scss удален за ненадобностью. Реализована основная логика по расчету и показу текущей позиции пользователя. Решено через событие клика

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,31 @@
 <template>
-  <div v-for="g in grid">{{ g }}</div>
+  <div id="positiveHeightMeter" class="heightMeter" v-if="isCurrentLayoutHasPositiveValue">
+    <span>
+      <span id="heightMeterNumber">{{ mousePosition }} </span>м над уровнем моря
+    </span>
+  </div>
+  <div id="negativeHeightMeter" class="heightMeter" v-else>
+    <span>
+      на <span id="heightMeterNumber">{{ mousePosition }} </span>м ниже уровня моря
+    </span>
+  </div>
+
+  <div
+    id="overgroundLayoutId"
+    class="backgroundLayout, overgroundLayout"
+    :hasPositiveValue="true"
+    :fromHeight="overgroundLayout.fromHeight"
+    :toHeight="overgroundLayout.toHeight"
+    :height="overgroundLayout.height"
+    @click="showOvergroundMousePosition"></div>
+  <div
+    id="undergroundLayoutId"
+    class="backgroundLayout, undergroundLayout"
+    :hasPositiveValue="false"
+    :fromHeight="undergroundLayout.fromHeight"
+    :toHeight="undergroundLayout.toHeight"
+    :height="undergroundLayout.height"
+    @click="showUndergroundPosition"></div>
 </template>
 
 <script>
@@ -7,8 +33,75 @@
     name: 'mainPage',
     data() {
       return {
-        grid: 1000,
+        isCurrentLayoutHasPositiveValue: true,
+        overgroundLayout: {
+          fromHeight: 0,
+          toHeight: 358000,
+          height: 358000,
+          heightPx: '358000px',
+        },
+        undergroundLayout: {
+          fromHeight: 0,
+          toHeight: -63570,
+          height: 63570,
+          heightPx: '63570px',
+        },
+        totalHeight: 421570,
+        mousePosition: 0,
       };
+    },
+    methods: {
+      showOvergroundMousePosition(event) {
+        this.setPositiveValueFlagByEvent(event);
+
+        let layerMousePosition = event.offsetY;
+        let layerElementToHeight = Number(event.target.getAttribute('toHeight'));
+        this.mousePosition = layerElementToHeight - layerMousePosition;
+      },
+      showUndergroundPosition(event) {
+        this.setPositiveValueFlagByEvent(event);
+
+        this.mousePosition = event.offsetY;
+      },
+      setPositiveValueFlagByEvent(event) {
+        this.isCurrentLayoutHasPositiveValue =
+          'true' === event.target.getAttribute('hasPositiveValue');
+      },
     },
   };
 </script>
+
+<style>
+  .heightMeter {
+    position: fixed;
+    margin-top: 20%;
+    top: 50%;
+    left: 15%;
+    right: 15%;
+    text-align: center;
+    font-size: x-large;
+    font-weight: bold;
+    opacity: 80%;
+    z-index: 100;
+  }
+
+  #heightMeterNumber {
+    font-size: xx-large;
+  }
+
+  .overgroundLayout {
+    height: v-bind('overgroundLayout.heightPx');
+    background-color: rgb(207, 222, 231);
+  }
+
+  .undergroundLayout {
+    height: v-bind('undergroundLayout.heightPx');
+    background-color: rgb(247, 235, 222);
+  }
+
+  .backgroundLayout {
+    position: absolute;
+    width: 100%;
+    z-index: 1;
+  }
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -9,7 +9,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@/styles/common.scss';
 
 global.jQuery = require('jquery');
 
